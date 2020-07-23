596015d2599b1756d6be1c21cd87bbc63986761a
Uma Sangumathi <UmaSangumathi@users.noreply.github.com>
2020-07-23 14:38:22 +0800

    Update celltype_specific_H3K4me3profile.R

--- celltype_specific_H3K4me3profile.R
+++ celltype_specific_H3K4me3profile.R
@@ -1,4 +1,3 @@
-### Function required to run EpiMogrify on a set of cell-types
 
 ## load libraries
 library(rtracklayer)
@@ -10,7 +9,7 @@ library(data.table)
 options(stringsAsFactors = FALSE)
 
 
-## Step I: Merge samples in each celltype. The union of broad peaks across samples are generated.
+## Merge samples in each celltype. The union of broad peaks across samples are generated.
 celltype_unionprofile <- function(peakdir, resdir, celltypes, qValue_cutoff ){
   extraCols_broadPeak <- c(signalValue = "numeric", pValue = "numeric", qValue = "numeric")
   for (ct in celltypes){
@@ -36,13 +35,13 @@ celltype_unionprofile <- function(peakdir, resdir, celltypes, qValue_cutoff ){
   }
 }
 
-
+## set directories
 hmdir="path"
 
-peakdir = paste0(hmdir, "/Data/samples/")
-resdir = paste0(hmdir, "Result")
+peakdir = paste0(hmdir, "/path_to_H3K4me3peakfiles/")
+resdir = paste0(hmdir, "/path_to_Resultdir")
 
-celltypes = c("day0__H7", "day9__cardiac_myoblast", "day14__cardiac_muscle_cell")
+celltypes = c("primary_cell__astrocytes", "primary_cell__cardiac_muscle_cell", "stem_cell__H9")
 qValue_cutoff = 5 ## -log10 pvalue = 10^-5
 
 dir.create(resdir)
